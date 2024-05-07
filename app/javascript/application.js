fb5a528ef668ee6c390a67717ddbf1c8de44794b
Alan <alan.figueroa@hotmail.com>
2024-05-06 22:55:30 -0600

    add tailwind css and login screen

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,24 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleThemeButton = document.getElementById("toggle-theme");
+
+  toggleThemeButton.addEventListener("click", () => {
+    const htmlElement = document.documentElement;
+
+    htmlElement.classList.toggle("dark");
+
+    const lightModeIcon = toggleThemeButton.querySelector(".light-mode");
+    const darkModeIcon = toggleThemeButton.querySelector(".dark-mode");
+
+    if (htmlElement.classList.contains("dark")) {
+      lightModeIcon.style.display = "none";
+      darkModeIcon.style.display = "inline";
+    } else {
+      lightModeIcon.style.display = "inline";
+      darkModeIcon.style.display = "none";
+    }
+  });
+});
